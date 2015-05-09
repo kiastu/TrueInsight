a2c06f6394d21da9bfa4941970b484bf13f29f6a
Tina Li <MingYuan@TinadeMacBook-Air-2.local>
2015-05-09 02:23:25 -0400

    EyeTribe functionality testing

---
+++ src/TETSimple.java
@@ -0,0 +1,44 @@
+/**
+ * Created by MingYuan on 5/9/15.
+ */
+
+import com.theeyetribe.client.GazeManager;
+import com.theeyetribe.client.IGazeListener;
+import com.theeyetribe.client.data.GazeData;
+
+
+public class TETSimple
+{
+    public static void main(String[] args)
+    {
+        final GazeManager gm = GazeManager.getInstance();
+        boolean success = gm.activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.PUSH);
+
+        final GazeListener gazeListener = new GazeListener();
+        gm.addGazeListener(gazeListener);
+
+        //TODO: Do awesome gaze control wizardry
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                gm.removeGazeListener(gazeListener);
+                gm.deactivate();
+            }
+        });
+    }
+
+    private static class GazeListener implements IGazeListener
+    {
+        @Override
+        public void onGazeUpdate(GazeData gazeData)
+        {
+            System.out.println(gazeData.toString());
+        }
+    }
+}
+
+
